54a7c408c4593bc423d5f7a56a27142001cc6395
reb2039607 <reb.aguayo@gmail.com>
2019-07-21 03:02:20 -0700

    updated crystal styling, created some variables, need to add click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,16 @@
 // alert("Hello!");
 // console.log("test");
+
+const wins = 0;
+const losses = 0;
+const counter = 0;
+const total = 0;
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (120 - 19) + 19);
+}
+
+//here's where I start to get a little lost
+start function () {
+$("#h2").append(getRandom);
+}
